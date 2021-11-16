ca67d95c7074838806ea1ee00cafb98244202407
Marisa Norton <marisanorton@mac-mini.home>
2021-11-15 19:14:43 -0600

    updated register.js

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const Register =()=>{
+const Register =({authenticateUser})=>{
+    let history = useHistory();
     const [userData, setUserData] = useState({
         name: "",
         email: "",
         password: "",
         passwordConfirm: ""
     });
-
+    const [errorData, setErrorData] = useState({errors: null});
     const {name, email, password, passwordConfirm } = userData;
+    const {errors} = errorData;
 
     const onChange = e => {
         const {name,value} = e.target;
@@ -19,8 +22,7 @@ const Register =()=>{
         });
     }
 
-    const register = async () => {
-
+    const registerUser = async () => {
         if(password !== passwordConfirm){
             console.log("Passwords do not match");
         }else{
@@ -29,7 +31,7 @@ const Register =()=>{
                 email: email,
                 password: password
             }
-             try {
+            try {
                 const config = {
                     headers: {
                         "Content-Type": "application/json"
@@ -38,14 +40,22 @@ const Register =()=>{
 
                 const body = JSON.stringify(newUser);
                 const res = await axios.post("http://localhost:5000/api/users", body,config);
-                console.log(res.data);
+
+                //store user data and redirect
+                localStorage.setItem("token", res.data.token);
+                history.push("/");
                 
                 } catch (error) {
-                    console.error(error.response.data);
-                    return;
+                    localStorage.removeItem("token");
+                    setErrorData({
+                        ...errors,
+                        errors: error.response.data.errors
+                    })
+                    
                 }
+
+                authenticateUser();
         }
-       
     }
     return(
         <div>
@@ -58,7 +68,7 @@ const Register =()=>{
                     value= {name}
                     onChange = {e => onChange(e)}/>
             </div>
-             <div>
+            <div>
                 <input 
                     type= "text"
                     placeholder="Email"
@@ -84,18 +94,13 @@ const Register =()=>{
                     onChange = {e => onChange(e)}/>
             </div>
             <div>
-                <button onClick = {()=> register()}>Register</button>
-            
+                <button onClick = {()=> registerUser()}>Register</button>
             </div>
-        
-        
-        
-        
-        
-        
+            <div>
+                {errors && errors.map(error => <div key = {error.msg}>{error.msg}</div>)}
+            </div>
         </div>
-
     )
-
 }
 export default Register;
+//
